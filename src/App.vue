7c1ec4af7004f497e149e2e8d59a4d6f81f62db0
OlgaGatalskaya <gatalolia86@gmail.com>
2025-10-24 19:35:26 +0200

    add component AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import AppHeader from './components/AppHeader.vue';
+
 </script>
 
 <template>
   <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
-    <h1>Shop</h1>
+    <AppHeader/>
   </div>
 </template>
