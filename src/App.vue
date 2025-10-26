6839e933b842f93f9828ebabf5e8efa0372640da
OlgaGatalskaya <gatalolia86@gmail.com>
2025-10-26 18:17:04 +0100

    add and layout component ShopCard

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue';
+import ShopCard from './components/ShopCard.vue';
 
 </script>
 
 <template>
-  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5  m-auto rounded-xl shadow-xl mt-14">
     <AppHeader/>
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Todas las zapatillas</h2>
+
+      <div class="grid grid-cols-4 gap-5">
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+      </div>
+    </div>
+    
   </div>
 </template>
---
+++ src/components/ShopCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
+        <img class="absolute top-8 left-8" src="/like-2.svg" alt="Like">
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker">
+        <p class="mt-2">Zapatillas Nike Blazer Mid Suede para hombre</p>
+        <div class="flex justify-between mt-5">
+          <div class="flex flex-col">
+            <span class="text-slate-400">Precio:</span>
+            <b>60 $</b>
+          </div>
+          <img src="/plus.svg" alt="Plus">
+        </div>
+      </div>
+</template>
